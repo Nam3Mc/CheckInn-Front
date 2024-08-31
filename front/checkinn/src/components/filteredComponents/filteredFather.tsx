0d16ec14a5d96d8b3fac92335469857ec6c080de
Juan Sebastian Lombo <lombofrancojuansebastian@gmail.com>
2024-08-31 20:25:18 -0300

    room filter

--- front/checkinn/src/components/filteredComponents/filteredFather.tsx
+++ front/checkinn/src/components/filteredComponents/filteredFather.tsx
@@ -1,12 +1,18 @@
-import React from "react";
+import React, { useState } from "react";
 import Filter from "./filter";
 import ProductsList from "./productsList";
 
-const FilteredFather = () => {
+const FilteredFather: React.FC = () => {
+  const [selectedOptions, setSelectedOptions] = useState<string[]>(['', '', '', '']);
+
   return (
-    <div className="p-5 w-[full] h-[90%]  flex gap-5 ">
-      <Filter></Filter>
-      <ProductsList></ProductsList>
+    <div className="x">
+      <div className=""> {/* Contenedor para el filtro */}
+        <Filter setSelectedOptions={setSelectedOptions} />
+      </div>
+      <div className="w-[100%] h-full overflow-auto m-5" > {/* Contenedor para las tarjetas */}
+        <ProductsList selectedOptions={selectedOptions} />
+      </div>
     </div>
   );
 };
