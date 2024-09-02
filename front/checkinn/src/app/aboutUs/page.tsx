5e8534f62035a40f5047303f77cf288f6dd7dc3a
Lucas_Pielvitori <pielvitorilucas@gmail.com>
2024-09-01 23:57:19 -0300

    a

--- front/checkinn/src/app/aboutUs/page.tsx
+++ front/checkinn/src/app/aboutUs/page.tsx
@@ -8,15 +8,32 @@ const AboutUs = () => {
   const teamMembers = team;
 
   return (
-    <div className="bg-greyVivino dark:bg-darkMode-greyVivino ">
-      <h1 className="text-wine text-center text-6xl font-plus-jakarta-sans  mb-8 p-20 pb-0">
-        Conoce más sobre nosotros
+    <div className="bg-greyVivino dark:bg-darkMode-greyVivino">
+      <h1 className="text-lavenderBlush text-center text-6xl font-plus-jakarta-sans mb-8 p-20 pb-0">
+        Learn More About Us
       </h1>
       <section className="my-10 flex flex-col items-center gap-8">
-        <h2 className="text-black dark:text-darkMode-white text-4xl text-center z-10">
-          Nuestro equipo
+        <h2 className="text-lavenderBlush dark:text-lavenderBlush text-4xl text-center z-10">
+          Welcome to CheckINN
         </h2>
-        <div className="flex gap-4  mt-32">
+        <p className="text-center text-lg text-gray-200 dark:text-gray-300 max-w-2xl mb-8 px-4">
+          At CheckINN, we blend comfort and tranquility with exceptional value,
+          making our hotel the perfect choice for both business travelers and
+          tourists alike. Strategically located in the heart of the city, we
+          offer modern spaces ideal for conventions, business meetings, and
+          corporate events, equipped with the latest technology to ensure the
+          success of every gathering.
+        </p>
+        <p className="text-center text-lg text-gray-200 dark:text-gray-300 max-w-2xl mb-8 px-4">
+          Additionally, our carefully curated dining options complement the
+          experience, allowing you to enjoy comprehensive events without leaving
+          our premises. Discover the excellence and dedication that define us,
+          and let us be a part of your next visit to [specific city].
+        </p>
+        <h2 className="text-lavenderBlush dark:text-lavenderBlush text-4xl text-center z-10">
+          Our Team
+        </h2>
+        <div className="flex gap-4 mt-32">
           {teamMembers.map((member: any) => (
             <CardTeam
               key={member.id}
--- front/checkinn/src/components/navbar/navbar.tsx
+++ front/checkinn/src/components/navbar/navbar.tsx
@@ -3,24 +3,27 @@
 import { usePathname } from "next/navigation";
 import { useEffect, useState } from "react";
 import Link from "next/link";
-import { LoginLogout } from "../loginLogout/LoginLogOut";
+import { FaUserCircle } from "react-icons/fa";
 
 export const NavBar: React.FC = () => {
   const pathname = usePathname();
-  const [token, setToken] = useState<string | null>(null);
+  const [isAuthenticated, setIsAuthenticated] = useState<boolean>(false);
 
   useEffect(() => {
     if (typeof window !== "undefined" && window.localStorage) {
+      // Verifica si existe el loginToken en localStorage
       const userToken = localStorage.getItem("loginToken");
-      setToken(userToken ? JSON.parse(userToken) : null);
+      const userData = localStorage.getItem("userDataLogin");
+
+      // Actualiza el estado si el token existe y está correctamente parseado
+      setIsAuthenticated(Boolean(userToken && userData));
     }
   }, [pathname]);
 
   return (
     <div
       className="fixed top-0 left-0 w-full z-50 bg-gray-900 shadow-md"
-      style={{ boxShadow: "0 4px 6px rgba(255, 255, 255, 0.3)" }}
-    >
+      style={{ boxShadow: "0 4px 6px rgba(255, 255, 255, 0.3)" }}>
       <nav className="pt-2">
         <div className="flex justify-between items-center p-3 pl-10 mx-large">
           <div>
@@ -63,22 +66,29 @@ export const NavBar: React.FC = () => {
             </ul>
           </div>
           <div className="flex space-x-2.5">
-  <Link href="/login">
-    <button className="bg-green-600 text-lavenderBlush hover:bg-green-400 focus:outline-none px-4 py-2 rounded">
-      Login
-    </button>
-  </Link>
-  <Link href="/register">
-    <button className="bg-blue-900 text-lavenderBlush hover:bg-blue-700 focus:outline-none px-4 py-2 rounded">
-      Register
-    </button>
-  </Link>
-</div>
-
+            {isAuthenticated ? (
+              <Link href="/profile">
+                {" "}
+                {/* Ruta para el perfil del usuario */}
+                <FaUserCircle className="text-lavenderBlush text-3xl cursor-pointer" />
+              </Link>
+            ) : (
+              <>
+                <Link href="/login">
+                  <button className="bg-green-600 text-lavenderBlush hover:bg-green-400 focus:outline-none px-4 py-2 rounded">
+                    Login
+                  </button>
+                </Link>
+                <Link href="/register">
+                  <button className="bg-blue-900 text-lavenderBlush hover:bg-blue-700 focus:outline-none px-4 py-2 rounded">
+                    Register
+                  </button>
+                </Link>
+              </>
+            )}
+          </div>
         </div>
       </nav>
     </div>
   );
 };
-
-{/* <LoginLogout /> */}
